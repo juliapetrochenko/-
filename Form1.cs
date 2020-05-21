493c0ca5433437db76656dcc6a28b5c9547c7b4c
https://github.com/juliapetrochenko/-.git <sm1121973@mail.ru>
2020-05-21 12:38:49 +0300

    init

---
+++ Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int x;
+            x = int.Parse(textBox1.Text);
+            if (x / 100 == x % 100 % 10)
+            {
+                label2.Text = "Ваше число является полиндромом";
+            }
+            else
+                label2.Text = "Ваше число не является полиндромом";
+
+            textBox1.ReadOnly = true;
+
+            button1.Visible = false;
+            button2.Visible = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+
+            label2.Text = "";
+
+            button1.Visible = true;
+            button2.Visible = false;
+
+            textBox1.ReadOnly = false;
+        }
+    }
+}
